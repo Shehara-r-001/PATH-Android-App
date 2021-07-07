39d965ef0f49466efe61fab5b06997a038727dd8
Shehara_r <ranathisarashehara01@gmail.com>
2021-07-07 15:53:17 +0530

    07-July

--- app/src/main/java/com/example/path_02/Login.java
+++ app/src/main/java/com/example/path_02/Login.java
@@ -2,30 +2,48 @@ package com.example.path_02;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Login extends AppCompatActivity {
+
+    private TextInputLayout email, password;
+    private Button login_btn;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() != null){
+            startActivity(new Intent(Login.this, User_Profile.class));
+        }
         setContentView(R.layout.activity_login);
 
-        Button login_btn = (Button) findViewById(R.id.login_btn);
-        login_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent next_act = new Intent(Login.this, User_Profile.class);
-                startActivity(next_act);
-            }
-        });
+        email = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
+        progressBar = findViewById(R.id.progressBar_login);
 
         Button register_page = (Button) findViewById(R.id.not_a_user);
         register_page.setOnClickListener(new View.OnClickListener() {
@@ -35,6 +53,51 @@ public class Login extends AppCompatActivity {
                 startActivity(reg_act);
             }
         });
+
+
+        login_btn = (Button) findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent next_act = new Intent(Login.this, User_Profile.class);
+//                startActivity(next_act);
+                String eMail = email.getEditText().toString();
+                final String passWord = password.getEditText().toString();
+
+                if(TextUtils.isEmpty(eMail)){
+                    Toast.makeText(getApplicationContext(), "Please enter your email address..!", Toast.LENGTH_LONG).show();
+                    return;
+                }if(TextUtils.isEmpty(passWord)){
+                    Toast.makeText(getApplicationContext(), "Please enter your password..!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+                auth.signInWithEmailAndPassword(eMail, passWord).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull /*@org.jetbrains.annotations.NotNull*/ Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+
+                        if(!task.isSuccessful()){
+                            if(passWord.length()<6){
+                                Toast.makeText(getApplicationContext(), "Your Password is too short..", Toast.LENGTH_LONG).show();
+                            }
+                            else{
+                                Toast.makeText(getApplicationContext(), "Authentication failed. Check your email and password and try again", Toast.LENGTH_LONG).show();
+
+                            }
+
+                        }
+                        else{
+                            Intent next_act = new Intent(Login.this, User_Profile.class);
+                            startActivity(next_act);
+                            finish();
+                        }
+                    }
+                });
+            }
+        });
+
 
     }
 
--- app/src/main/java/com/example/path_02/User_Register.java
+++ app/src/main/java/com/example/path_02/User_Register.java
@@ -2,21 +2,31 @@ package com.example.path_02;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class User_Register extends AppCompatActivity {
 
     TextInputLayout reg_Fname, reg_Uname, reg_email, reg_Pnum, reg_pword, reg_Rpword;
     Button reg_btn, to_login;
+    ProgressBar progressBar;
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +35,16 @@ public class User_Register extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.register);
 
+        auth = FirebaseAuth.getInstance();
         reg_Fname = findViewById(R.id.signup_fullname);
         reg_Uname = findViewById(R.id.signup_username);
         reg_email = findViewById(R.id.signup_email);
         reg_Pnum = findViewById(R.id.signup_contact);
         reg_pword = findViewById(R.id.signup_password);
         reg_Rpword = findViewById(R.id.signup_retype_password);
+        progressBar = findViewById(R.id.progressBar);
 
-        reg_btn = (Button) findViewById(R.id.reg_btn);
-        reg_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-//                Intent next_act = new Intent(User_Register.this, User_Profile.class);
-//                startActivity(next_act);
-            }
-        });
         to_login = (Button) findViewById(R.id.to_login_page);
         to_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,5 +62,64 @@ public class User_Register extends AppCompatActivity {
 //        Spinner spinner01 = (Spinner) findViewById(R.id.category_spinner);
 //        spinner01.setOnItemSelectedListener(User_Register.this);
 
+
+        reg_btn = (Button) findViewById(R.id.reg_btn);
+        reg_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Intent next_act = new Intent(User_Register.this, User_Profile.class);
+//                startActivity(next_act);
+                String full_name = reg_Fname.getEditText().toString().trim();
+                String user_name = reg_Uname.getEditText().toString().trim();
+                String email = reg_email.getEditText().toString().trim();
+                String password = reg_pword.getEditText().toString().trim();
+
+                if (TextUtils.isEmpty(full_name)) {
+                    Toast.makeText(getApplicationContext(), "Please enter your full name..!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(user_name)) {
+                    Toast.makeText(getApplicationContext(), "Please enter a user name..!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Please enter your email address..!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Please enter a password..!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Your password id too short..!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(User_Register.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull /*@org.jetbrains.annotations.NotNull*/ Task<AuthResult> task) {
+                        Toast.makeText(User_Register.this, "createUserWithEmail:onComplete" + task.isSuccessful(), Toast.LENGTH_LONG).show();
+                        progressBar.setVisibility(View.GONE);
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(User_Register.this, "Authentication Failed..!" + task.getException(), Toast.LENGTH_LONG).show();
+
+                        } else {
+                            startActivity(new Intent(User_Register.this, User_Profile.class));
+                            finish();
+                        }
+                    }
+                });
+
+            }
+        });
+
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
     }
 }
